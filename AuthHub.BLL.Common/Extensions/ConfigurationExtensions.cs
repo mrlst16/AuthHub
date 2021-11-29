e4ad4677c610c67b741eb8c0eaa8c568dc643c2e
LAPTOP-R0OK2Q7S\matt <mrlst16@mail.rmu.edu>
2021-11-28 18:09:35 -0800

    update packages

---
+++ AuthHub.BLL.Common/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace AuthHub.BLL.Common.Extensions
+{
+    public static class ConfigurationExtensions
+    {
+        public static Guid AuthHubOrganizationId(this IConfiguration configuration)
+            => configuration.GetValue<Guid>("AppSettings:AuthHubOrganiztionID");
+        public static string AuthHubIssuer(this IConfiguration configuration)
+            => configuration.GetValue<string>("AppSettings:AuthHubIssuer");
+        public static string AuthHubKey(this IConfiguration configuration)
+            => configuration.GetValue<string>("AppSettings:AuthHubKey");
+
+        public static (Guid, string, string) AuthHubAuthInfo(this IConfiguration configuration)
+            => (configuration.AuthHubOrganizationId(), configuration.AuthHubIssuer(), configuration.AuthHubKey());
+    }
+}
