d26f8b74495a9d15c044fd5c0905cd245459d384
matt <mrlst16@mail.rmu.edu>
2022-11-07 21:19:29 -0800

    Add error codes, fix messages

---
+++ AuthHub/APIErrorCodes.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthHub
+{
+    public static class APIErrorCodes
+    {
+        public const string FieldMissing = "000001";
+        public const string UsernameUnavailable = "000002";
+        public const string PasswordLengthTooShort = "000003";
+    }
+}
--- AuthHub/Validators/CreateUserRequestValidator.cs
+++ AuthHub/Validators/CreateUserRequestValidator.cs
@@ -17,28 +17,37 @@ namespace AuthHub.Validators
             RuleFor(x => x.UserName)
                 .NotNull()
                 .NotEmpty()
-                .WithMessage("Username cannot be empty");
+                .WithMessage("Username cannot be empty")
+                .WithErrorCode(APIErrorCodes.FieldMissing);
 
             RuleFor(x => x.Password)
                 .NotNull()
                 .NotEmpty()
-                .WithMessage("Password cannot be empty");
+                .WithMessage("Password cannot be empty")
+                .WithErrorCode(APIErrorCodes.FieldMissing);
 
             RuleFor(x => x.FirstName)
                 .NotNull()
                 .NotEmpty()
-                .WithMessage("FirstName cannot be empty");
-
-            RuleFor(x => x.Password)
-                .Must(x => x.Length >= 8);
+                .WithMessage("FirstName cannot be empty")
+                .WithErrorCode(APIErrorCodes.FieldMissing);
 
             RuleFor(x => x.Email)
                 .NotNull()
                 .NotEmpty()
-                .WithMessage("Email cannot be empty");
+                .WithMessage("Email cannot be empty")
+                .WithErrorCode(APIErrorCodes.FieldMissing);
+
+            RuleFor(x => x.Password)
+                .Must(x => x.Length >= 8)
+                .WithMessage("Password must be at least eight characters long")
+                .WithErrorCode(APIErrorCodes.PasswordLengthTooShort);
+
 
             RuleFor(x => x.UserName)
-                .MustAsync(async (x, c) => await UserNameAvailaible(x));
+                .MustAsync(async (x, c) => await UserNameAvailaible(x))
+                .WithMessage("Username is already taken")
+                .WithErrorCode(APIErrorCodes.UsernameUnavailable);
         }
 
         public async Task<bool> UserNameAvailaible(string userName)
@@ -46,7 +55,6 @@ namespace AuthHub.Validators
             try
             {
                 var response = await _userLoader.GetAsync(userName);
-
                 return response == null;
             }
             catch (Exception e)
