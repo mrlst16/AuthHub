122400be56a5637e397c08a6d58ab815f28837e9
matt <mrlst16@mail.rmu.edu>
2024-11-21 01:33:07 -0500

    experiment with jobs

---
+++ AuthHub.Jobs/Jobs/BillingJob.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hangfire;
+
+namespace AuthHub.Jobs.Jobs
+{
+    public class BillingJob
+    {
+        public static void Run()
+        {
+            BackgroundJob.Enqueue(() => Console.WriteLine($"The current date in UTC is {DateTime.UtcNow}"));
+        }
+
+        public void RunThis(string jobName)
+        {
+
+        }
+    }
+}
--- AuthHub.Jobs/Program.cs
+++ AuthHub.Jobs/Program.cs
@@ -1,11 +1,42 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using AuthHub.Jobs.Jobs;
+using Hangfire;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 namespace AuthHub.Jobs
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            HostApplicationBuilder app = new HostApplicationBuilder();
+            app.Configuration.AddJsonFile("appsettings.json", false)
+                .AddJsonFile($"appsettings.{env}.json", true);
+
+            Console.WriteLine("Starting...");
+            try
+            {
+                GlobalConfiguration.Configuration
+                    .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+                    .UseSimpleAssemblyNameTypeSerializer()
+                    .UseRecommendedSerializerSettings()
+                    .UseSqlServerStorage(app.Configuration.GetConnectionString("hangfire"));
+
+                //RecurringJob
+                //    .AddOrUpdate(() => BillingJob.Run(),
+                //        "*/1 * * * * *"
+                //        );
+                using (var server = new BackgroundJobServer())
+                {
+                    Console.ReadKey();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
     }
 }
